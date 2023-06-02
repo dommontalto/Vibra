4e98380ee1c8357a50805b31438529e9d3b66eaa
Dom Montalto <dommontalto@outlook.com>
2023-06-02 17:02:34 +0100

    Spotify connected

--- Vibra/Managers/AuthManager.swift
+++ Vibra/Managers/AuthManager.swift
@@ -10,8 +10,21 @@ import Foundation
 final class AuthManager {
     static let shared = AuthManager()
     
+    struct Constants {
+        static let clientID = "2c27bb2b230a489b95dc35382a890020"
+        static let clientSectret = "f276b209521a4e0cad91102908e177ed"
+    }
+    
     private init(){}
     
+    public var signInURL: URL?{
+        let scopes = "user-read-private"
+        let redirectURI = "https://www.iosacademy.io"
+        let base = "https://accounts.spotify.com/authorize"
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope-\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        return URL(string: string)
+    }
+                              
     var isSingedIn: Bool {
         return false
     }
@@ -30,5 +43,17 @@ final class AuthManager {
     
     private var shouldRefreshToken: Bool {
         return false
+    }
+    
+    public func exchangeCodeForToken(code: String, completion: @escaping(Bool) -> Void){
+        
+    }
+    
+    public func refreshToken(){
+        
+    }
+    
+    private func cacheToken(){
+        
     }
 }
